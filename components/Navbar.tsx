dc66efaea2055c8a17fddbd9fd0023f6eedf7d94
brtkn <haha>
2023-07-07 12:12:43 +0300

    second commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,12 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import AuthProviders from "./AuthProviders";
+import { getCurrentUser } from "@/lib/sessions";
+import { signOut } from "next-auth/react";
+import ProfileMenu from "./ProfileMenu";
 
-const Navbar = () => {
-  const session = {};
+const Navbar = async () => {
+  const session = await getCurrentUser();
 
   return (
     <nav className="flexBetween navbar">
@@ -22,9 +25,9 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="flexCenter gap-4">
-        {session ? (
+        {session?.user ? (
           <>
-            User Photo
+            <ProfileMenu session={session} />
             <Link href="/create-project"> Share Work</Link>
           </>
         ) : (
